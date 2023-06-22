63f1c3c02f61def2336019cc4d5a9e344c8900e1
Dina-72 <amenova1109@yandex.ru>
2023-06-22 23:26:26 +0500

    changes commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -3,11 +3,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
     @Test
-    public void findMax() {
+    public void findMax1() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 23, 8, 6, 11, 11, 12};
+        long expected = 23;
 
         long actual = service.findMax(incomesInBillions);
 
